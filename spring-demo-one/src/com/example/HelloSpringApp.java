3200551034a0b38c39cc924b76baaaf21ee340f8
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-04-26 23:46:13 -0400

    adding spring container

---
+++ spring-demo-one/src/com/example/HelloSpringApp.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml"); 
+		
+		// retrieve bean from spring container
+		Coach theCoatch = context.getBean("myCoach", Coach.class);
+		
+		// call methods on the bean
+		System.out.println(theCoatch.getDailyWorkout());
+		
+		// close the context
+		context.close();
+		
+	}
+
+}
