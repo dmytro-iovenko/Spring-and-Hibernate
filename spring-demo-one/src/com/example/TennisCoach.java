0e445b69ebe5f1f2699142d438ffe1b787d1ef79
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-04-27 00:01:56 -0400

    adding TennisCoach

---
+++ spring-demo-one/src/com/example/TennisCoach.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Perform rear foot elevated squats 15 times";
+	}
+
+}
