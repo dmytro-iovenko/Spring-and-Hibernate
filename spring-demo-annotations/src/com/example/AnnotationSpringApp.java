27cae93a815c2bf83994e54fb6c6c58294edf270
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-05-03 23:00:33 -0400

    injecting properties file with annotations

--- spring-demo-annotations/src/com/example/AnnotationSpringApp.java
+++ spring-demo-annotations/src/com/example/AnnotationSpringApp.java
@@ -11,7 +11,7 @@ public class AnnotationSpringApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml"); 
 		
 		// get the bean from spring container
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		TennisCoach theCoach = context.getBean("tennisCoach", TennisCoach.class);
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
@@ -19,6 +19,10 @@ public class AnnotationSpringApp {
 		// call a method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
 		
+		// call methods to get literal values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
+
 		// close the context
 		context.close();
 
--- spring-demo-annotations/src/com/example/TennisCoach.java
+++ spring-demo-annotations/src/com/example/TennisCoach.java
@@ -2,14 +2,21 @@ package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	
 	@Autowired
-	@Qualifier("happyFortuneService")
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
+
+	@Value("${foo.email}")
+	private String emailAddress;
+
+	@Value("${foo.team}")
+	private String team;
 	
 	// define a default constructor
 	public TennisCoach() {
@@ -30,6 +37,14 @@ public class TennisCoach implements Coach {
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
 	}
 
 	@Override
