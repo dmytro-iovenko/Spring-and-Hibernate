704281c17a8f2cd9ea1567bc7ef8ef76a52a77c8
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-05-04 23:27:36 -0400

    add java configuration file

---
+++ spring-demo-annotations/src/com/example/JavaConfigSpringApp.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigSpringApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(SportConfig.class); 
+		
+		// get the bean from spring container
+		TennisCoach theCoach = context.getBean("tennisCoach", TennisCoach.class);
+		
+		// call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+
+		// call a method to get the daily fortune
+		System.out.println(theCoach.getDailyFortune());
+		
+		// call methods to get literal values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
+
+		// close the context
+		context.close();
+
+	}
+
+}
