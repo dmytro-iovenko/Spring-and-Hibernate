d4a02d127674a7ec741572c8fcbebd85f4e122f2
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-05-09 22:56:29 -0400

    binding request params

--- spring-demo-mvc/src/com/example/mvc/HelloWorldController.java
+++ spring-demo-mvc/src/com/example/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -12,7 +13,7 @@ public class HelloWorldController {
 	// need a controller method to show the initial HTML form
 	@RequestMapping("/showForm")
 	public String showForm() {
-		return "hello-form";
+		return "helloworld-form";
 	}
 	
 	// need a controller method to process the HTML form
@@ -34,6 +35,23 @@ public class HelloWorldController {
 		
 		// create the message
 		String result = "Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+				
+		return "helloworld";
+	}
+
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey My Friend! " + theName;
 		
 		// add message to the model
 		model.addAttribute("message", result);
