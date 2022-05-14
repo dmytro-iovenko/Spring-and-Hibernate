2d3e23fac7b661ca194fcaaf7ce0012ddeedf897
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-05-14 00:32:14 -0400

    make Integer field required and handle Strings

--- spring-demo-mvc/src/com/example/mvc/Customer.java
+++ spring-demo-mvc/src/com/example/mvc/Customer.java
@@ -1,15 +1,26 @@
 package com.example.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
-
+	
 	private String firstName;
 	
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
+
+	@NotNull(message="is required")
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than or equal to 10")
+	private Integer freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
 
 	public String getFirstName() {
 		return firstName;
@@ -27,13 +38,20 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
 }
-
-
-
-
-
-
-
-
