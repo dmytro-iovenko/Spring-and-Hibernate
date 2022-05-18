6a8a04b44e14eef904b48bbf603b5a2ccaf46945
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-05-18 00:04:28 -0400

    adding custom validation with multiple strings

--- spring-demo-mvc/src/com/example/mvc/validation/CourseCodeConstraintValidator.java
+++ spring-demo-mvc/src/com/example/mvc/validation/CourseCodeConstraintValidator.java
@@ -5,7 +5,7 @@ import javax.validation.ConstraintValidatorContext;
 
 public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
 
-	private String coursePrefix;
+	private String[] coursePrefix;
 	
 	@Override
 	public void initialize(CourseCode theCourseCode) {
@@ -17,7 +17,18 @@ public class CourseCodeConstraintValidator implements ConstraintValidator<Course
 		if (theCode == null) {
 			return true;
 		}
-		boolean result = theCode.startsWith(coursePrefix);
+		boolean result = false;
+
+		// loop thru course prefixes
+		// and check if code matches any of the course prefixes 
+		for (String prefix : coursePrefix) {
+			result = theCode.startsWith(prefix);
+
+			// if we found a match then break out of the loop
+			if(result) {
+				break;
+			}
+		}
 		return result;
 	}
 
