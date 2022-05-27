9fa6b16055b0a86cb507833bb55fda2c36ed6005
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-05-26 23:35:38 -0400

    update Instructor mapping

--- hibernate-one-to-many/src/com/example/hibernate/demo/entity/Instructor.java
+++ hibernate-one-to-many/src/com/example/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.example.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -39,6 +43,11 @@ public class Instructor {
 	private InstructorDetail instructorDetail;
 	
 	
+	@OneToMany(mappedBy="instructor",
+			   cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+						 CascadeType.DETACH, CascadeType.REFRESH})
+	private List<Course> courses;
+
 	// create constructors
 	public Instructor() {
 		
@@ -92,6 +101,27 @@ public class Instructor {
 		this.instructorDetail = instructorDetail;
 	}
 	
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+	// add convenience methods for bi-directional relationship
+	
+	public void add(Course tempCourse) {
+		
+		if (courses == null) {
+			courses = new ArrayList<>();
+		}
+		
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
+	}
+	
 	// generate toString() method
 
 	@Override
@@ -99,6 +129,5 @@ public class Instructor {
 		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", instructorDetail=" + instructorDetail + "]";
 	}
-	
-	
+
 }
