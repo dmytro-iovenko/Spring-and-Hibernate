014c89778adb7c28f1614dd66a2752a7cd9acf5e
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-06-07 23:27:26 -0400

    adding service layer

--- web-customer-tracker/src/com/example/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/example/springdemo/controller/CustomerController.java
@@ -8,22 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.springdemo.dao.CustomerDAO;
 import com.example.springdemo.entity.Customer;
+import com.example.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-	// need to inject the customer dao
+	// need to inject our customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
-		// get customers from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		// get customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 				
 		// add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
---
+++ web-customer-tracker/src/com/example/springdemo/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.example.springdemo.service;
+
+import java.util.List;
+
+import com.example.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+}
