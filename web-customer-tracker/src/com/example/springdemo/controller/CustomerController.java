61b587a172e6f83c67dada70563e54094373512f
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-06-08 23:49:35 -0400

    save customer to database

--- web-customer-tracker/src/com/example/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/example/springdemo/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.springdemo.entity.Customer;
@@ -44,4 +46,14 @@ public class CustomerController {
 
 	}
 	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		// save the customer using our service
+		customerService.saveCustomer(theCustomer);
+		
+		return "redirect:/customer/list";
+
+	}
+
 }
