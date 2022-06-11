4096b8475a1cc16da654a40d170330cf20017efe
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-06-10 23:21:34 -0400

    deleting data from database

--- web-customer-tracker/src/com/example/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/example/springdemo/controller/CustomerController.java
@@ -72,4 +72,14 @@ public class CustomerController {
 
 	}
 
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int theId) {
+		
+		// delete the customer
+		customerService.deleteCustomer(theId);
+		
+		return "redirect:/customer/list";
+
+	}
+
 }
