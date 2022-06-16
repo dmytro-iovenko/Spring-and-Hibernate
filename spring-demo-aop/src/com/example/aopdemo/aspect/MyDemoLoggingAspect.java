a5c48ae03cc97e7cfab959daee59ea8ee3dfc165
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-06-15 23:56:28 -0400

    combining poincut expressions

--- spring-demo-aop/src/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -16,12 +16,24 @@ public class MyDemoLoggingAspect {
 	@Pointcut("execution(* com.example.aopdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
-	@Before("forDaoPackage()")
+	// create pointcut for getter methods
+	@Pointcut("execution(* com.example.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+	
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.example.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	// create pointcut: include package ... exclude getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
+	
+	@Before("forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {		
 		System.out.println("\n=====>>> Executing @Before advice on method");		
 	}
 	
-	@Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
 	public void performApiAnalytics() {
 		System.out.println("\n=====>>> Performing API analytics");		
 	}
