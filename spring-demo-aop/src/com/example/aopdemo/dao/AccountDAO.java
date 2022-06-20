d2d5de78a14c668c0e6b9efeb2ee89eb449e373e
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-06-19 22:56:51 -0400

    adding @AfterThrowuing advice

--- spring-demo-aop/src/com/example/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop/src/com/example/aopdemo/dao/AccountDAO.java
@@ -15,7 +15,12 @@ public class AccountDAO {
 	
 	// add a new method: findAccounts()
 	
-	public List<Account> findAccounts() {
+	public List<Account> findAccounts(boolean tripWire) {
+		
+		// for academic purpose ... simulate an exception
+		if (tripWire) {
+			throw new RuntimeException("No soup for you!!!");
+		}	
 		
 		List<Account> myAccounts = new ArrayList<>();
 		
@@ -28,7 +33,6 @@ public class AccountDAO {
 		myAccounts.add(temp1);
 		myAccounts.add(temp2);
 		myAccounts.add(temp3);
-		
 		
 		return myAccounts;		
 	}
