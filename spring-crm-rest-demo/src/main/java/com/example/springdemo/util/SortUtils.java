3ea33654234fd62e5de45a08148c541474a9563d
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-07-12 23:52:21 -0400

    initial commit

---
+++ spring-crm-rest-demo/src/main/java/com/example/springdemo/util/SortUtils.java
@@ -0,0 +1,11 @@
+package com.example.springdemo.util;
+
+public interface SortUtils {
+
+	public static final int FIRST_NAME = 1;
+
+	public static final int LAST_NAME = 2;
+
+	public static final int EMAIL = 3;
+
+}
