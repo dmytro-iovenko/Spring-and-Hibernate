5006b5c7a56c59a46f2a79c1823cb3e7807a02b3
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-07-19 23:19:29 -0400

    securing rest api endpoints

---
+++ spring-crm-rest-demo/src/main/java/com/example/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.example.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
