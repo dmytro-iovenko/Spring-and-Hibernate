6e909fde4d5987e27e6408a0bcf14d86fb2291be
Liudmyla Iovenko <86856650+liudmylai@users.noreply.github.com>
2022-08-07 23:05:27 -0400

    add employee

--- thymeleaf-demo/src/main/java/com/example/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ thymeleaf-demo/src/main/java/com/example/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.example.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springboot.thymeleafdemo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
 }
